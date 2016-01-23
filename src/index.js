20e7ab6a6b8105eb4b14e6c87bb0b86311b242fb
Ari Porad <ari@ariporad.com>
2016-01-23 15:28:17 -0800

    feat: export `createParser` and the version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 /* (c) 2016 EMIW, LLC. emiw.xyz/license */
-// import pkg from '../package.json';
+import pkg from '../package.json';
 
 // We need a default version b/c semantic-release doesn't let us have a version unless it's the
 // released version.
-// TODO: Is this the development version that we should have? Maybe we should change it to `dev`?
-// Or `999.999.999-dev`?
-// const { version = /* istanbul ignore next: impossible to test */ '0.0.0-dev' } = pkg;
+// TODO: Is this the development version that we want? Should we change it to `999.999.999-dev`?
+/* istanbul ignore next: untestable */
+const version = pkg.version || '0.0.0-dev';
 
-// export { default as createParser } from './Parser';
-// export { encode, decode } from './parse';
-// export { version };
+export { default as createParser } from './Parser';
+export { version };
--- test/index.js
+++ test/index.js
@@ -1,19 +1,13 @@
 /* (c) 2016 EMIW, LLC. emiw.xyz/license */
 import test from 'ava';
-
-test('noop', t => t.ok(true));
-
-/*
 import * as index from '..';
+import { valid } from 'semver';
 import createParser from '../dist/Parser';
-import { encode, decode } from '../dist/parse';
 
 test('createParser', t => {
-  t.is(index.createParser, createParser);
+  t.is(index.createParser, createParser, 'proper createParser');
 });
 
-test('encode/decode', t => {
-  t.is(index.encode, encode);
-  t.is(index.decode, decode);
+test('valid version', t => {
+  t.ok(valid(index.version), 'valid version');
 });
-*/
